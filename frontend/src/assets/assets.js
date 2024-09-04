080a2e44eb4737d775ad491dd49c2e54d4c94a13
laith12khater <laithkhater12@gmail.com>
2024-09-04 12:59:13 +0300

    images

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,22 @@
+import app_store from './app_store.png';
+import cross_icon from './cross_icon.png'
+import facebook_icon from './facebook_icon.png'
+import header_img from './header_img.png'
+import linkedin_icon from './linkedin_icon.png'
+import logo from './logo.png'
+import play_store from './play_store.png'
+import twitter_icon from './twitter_icon.png'
+import logo2 from './logo2.png'
+
+
+export const assets = {
+    logo,
+    app_store,
+    cross_icon,
+    facebook_icon,
+    header_img,
+    linkedin_icon,
+    play_store,
+    twitter_icon,
+    logo2
+}
